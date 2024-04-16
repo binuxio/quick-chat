f2053789869c48e692cfab128d0b75b0b67f0354
bilo <bilo.dev7@gmail.com>
2024-04-16 02:27:23 +0200

    added vite_server_host to .env

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ dotenv.config();
 
 const SERVER_ADDRESS = process.env.SERVER_ADDRESS || 'http://localhost:4050';
 const VITE_SERVER_PORT = parseInt(process.env.VITE_SERVER_PORT) || 3080;
+const VITE_SERVER_HOST = process.env.VITE_SERVER_HOST || "localhost";
 
 export default defineConfig({
   plugins: [react()],
@@ -14,7 +15,7 @@ export default defineConfig({
     }
   },
   server: {
-    host: "0.0.0.0",
+    host: VITE_SERVER_HOST,
     port: VITE_SERVER_PORT,
     proxy: {
       '/api': {
